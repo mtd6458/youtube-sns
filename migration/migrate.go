fca6c86a24b3ceb4b1484efab8ab56cfb4f4314e
mtd6458 <md.yu.03@gmail.com>
2021-06-19 21:31:40 +0900

    マイグレーションファイル追加

---
+++ migration/migrate.go
@@ -0,0 +1,29 @@
+package migration
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// Migrate program.
+func Migrate() {
+
+	// データベースをオープン
+	// 変数1, 変数2 := gorm.Open( ドライバ名 , データベース名 )
+	// 変数1にはGORMに用意されているDB構造体が格納される
+	db, er := gorm.Open("sqlite3", "data.sqlite3")
+
+	if er != nil {
+		fmt.Println(er)
+		return
+	}
+
+	defer db.Close()
+
+	// DB構造体からマイグレーション実行用のメソッドを呼び出す
+	// 引数にはマイグレーションするモデルの構造体の値をポインタで渡す。
+	// いくつでも指定できる。
+	db.AutoMigrate(&User{}, &Group{}, &Post{}, &Comment{})
+}
