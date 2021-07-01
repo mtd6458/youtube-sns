403aeafb22e89881197401cb3abf56b2ac43ea2c
mtd6458 <md.yu.03@gmail.com>
2021-07-01 11:40:44 +0900

    update Build using musl-cross to run the cgo program.

--- sh/upload.sh
+++ sh/upload.sh
@@ -1,7 +1,7 @@
 #!bin/sh
 
 # osをlinux、ARCHをamd64(x86_64)にしてbuild
-GOOS=linux GOARCH=amd64 go build -o main_8080
+GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=/usr/local/bin/x86_64-linux-musl-cc go build --ldflags '-linkmode external -extldflags "-static"' -a -v -o main_8080
 
 scp main_8080 you-tube-sns:~/app/
 
