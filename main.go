b531222411cfe32bfe69112ac14e372f262fcc19
mtd6458 <md.yu.03@gmail.com>
2021-07-23 23:37:53 +0900

    update Auth0で必要なメソッド追加

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/coreos/go-oidc"
 	"github.com/gorilla/sessions"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -9,8 +12,13 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"strconv"
 	"strings"
+
+	"app"
+	"auth"
 )
 
 // db variable.
@@ -21,15 +29,24 @@ var sessionName = "ytboard-session"
 var cs = sessions.NewCookieStore([]byte("secret-key-1234"))
 
 func main() {
-	fmt.Println("http://localhost:8080/")
+	app.Init()
+
+	log.Print("Server listening on http://localhost:8080/")
 
 	/**
 	 * routing
 	 */
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", IndexHandler)
 
+	http.HandleFunc("/login", LoginHandler)
 
-	http.HandleFunc("/home", home)
+	http.HandleFunc("/callback", CallbackHandler)
+
+	http.HandleFunc("/logout", LogoutHandler)
+
+	http.HandleFunc("/top", TopHandler)
+
+	http.HandleFunc("/home", HomeHandler)
 
 	http.HandleFunc("/post", post)
 
@@ -61,9 +78,148 @@ func page(fname string) *template.Template {
 	return templates
 }
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	page("index").Execute(w, nil)
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	// Generate random state
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	state := base64.StdEncoding.EncodeToString(b)
+
+	session, err := app.Store.Get(r, "auth-session")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	session.Values["state"] = state
+	err = session.Save(r, w)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	authenticator, err := auth.NewAuthenticator()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, authenticator.Config.AuthCodeURL(state), http.StatusTemporaryRedirect)
+}
+
+func CallbackHandler(w http.ResponseWriter, r *http.Request) {
+
+	session, err := app.Store.Get(r, "auth-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if r.URL.Query().Get("state") != session.Values["state"] {
+		http.Error(w, "Invalid state parameter", http.StatusBadRequest)
+		return
+	}
+
+	authenticator, err := auth.NewAuthenticator()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	token, err := authenticator.Config.Exchange(context.TODO(), r.URL.Query().Get("code"))
+	if err != nil {
+		log.Printf("no token found: %v", err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	rawIDToken, ok := token.Extra("id_token").(string)
+	if !ok {
+		http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+		return
+	}
+
+	oidcConfig := &oidc.Config{
+		ClientID: os.Getenv("AUTH0_CLIENT_ID"),
+	}
+
+	idToken, err := authenticator.Provider.Verifier(oidcConfig).Verify(context.TODO(), rawIDToken)
+
+	if err != nil {
+		http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Getting now the userInfo
+	var profile map[string]interface{}
+	if err := idToken.Claims(&profile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["id_token"] = rawIDToken
+	session.Values["access_token"] = token.AccessToken
+	session.Values["profile"] = profile
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Redirect to logged in page
+	http.Redirect(w, r, "/top", http.StatusSeeOther)
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+
+	domain := os.Getenv("AUTH0_DOMAIN")
+
+	logoutUrl, err := url.Parse("https://" + domain)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	logoutUrl.Path += "/v2/logout"
+	parameters := url.Values{}
+
+	var scheme string
+	if r.TLS == nil {
+		scheme = "http"
+	} else {
+		scheme = "https"
+	}
+
+	returnTo, err := url.Parse(scheme + "://" + r.Host)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	parameters.Add("returnTo", returnTo.String())
+	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
+	logoutUrl.RawQuery = parameters.Encode()
+
+	http.Redirect(w, r, logoutUrl.String(), http.StatusTemporaryRedirect)
+}
+
 // top page handler
-func index(w http.ResponseWriter, rq *http.Request) {
-	user := checkLogin()
+func TopHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := app.Store.Get(r, "auth-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println(session.Values["profile"])
 
 	db, _ := gorm.Open(dbDriver, dbName)
 	defer db.Close()
@@ -81,7 +237,7 @@ func index(w http.ResponseWriter, rq *http.Request) {
 		TagList  []migration.Tag
 	}{
 		Title:    "Index",
-		UserName: user.Name,
+		UserName: "",
 		PostList: postList,
 		TagList:  tagList,
 	}
@@ -104,7 +260,7 @@ func logout(w http.ResponseWriter, request *http.Request) {
 }
 
 // home page handler
-func home(writer http.ResponseWriter, request *http.Request) {
+func HomeHandler(writer http.ResponseWriter, request *http.Request) {
 	user := checkLogin()
 
 	db, _ := gorm.Open(dbDriver, dbName)
