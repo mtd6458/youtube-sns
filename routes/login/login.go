dc866a2face63cad2a81b5a1296c30a1412fd45c
mtd6458 <md.yu.03@gmail.com>
2021-08-10 01:23:58 +0900

    add Login.go

---
+++ routes/login/login.go
@@ -0,0 +1,39 @@
+package login
+
+import (
+  "app"
+  "auth"
+  "crypto/rand"
+  "encoding/base64"
+  "net/http"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+  // Generate random state
+  b := make([]byte, 32)
+  _, err := rand.Read(b)
+
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  state := base64.StdEncoding.EncodeToString(b)
+
+  session, _ := app.Store.Get(r, "auth-session")
+  session.Values["state"] = state
+  err = session.Save(r, w)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  authenticator, err := auth.NewAuthenticator()
+
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  http.Redirect(w, r, authenticator.Config.AuthCodeURL(state), http.StatusTemporaryRedirect)
+}
